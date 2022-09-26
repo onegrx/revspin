b02c68bcd88f9571205614fd0fcd9b4b8f15069d
Bartłomiej Szałach <onegrx@gmail.com>
2022-09-26 05:14:42 +0200

    Process concurrently using Task.async_stream

--- lib/revspin/processor.ex
+++ lib/revspin/processor.ex
@@ -14,7 +14,7 @@ defmodule Revspin.Processor do
     html
     |> Parser.parse_brands_blades_page()
     # Comment the next line for real use
-    |> Enum.take(4)
+    |> Enum.take(10)
     |> Enum.each(&process_brand_with_blades/1)
   end
 
@@ -26,8 +26,9 @@ defmodule Revspin.Processor do
 
     blades
     # Comment the next line for real use
-    |> Enum.take(4)
-    |> Enum.each(fn blade -> process_blade(blade, inserted_brand) end)
+    |> Enum.take(20)
+    |> Task.async_stream(fn blade -> process_blade(blade, inserted_brand) end)
+    |> Enum.to_list
   end
 
   defp process_blade(%{link: link, name: blade_name}, inserted_brand) do
