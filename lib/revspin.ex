a1fc5f7725b9ed25c264bc5865c9e379158230e0
Bartłomiej Szałach <onegrx@gmail.com>
2022-09-30 02:51:52 +0200

    Change main function name to run

--- lib/revspin.ex
+++ lib/revspin.ex
@@ -1,8 +1,8 @@
 defmodule Revspin do
   @moduledoc """
     Provides an API to fetch, process and save tennis blades with brands from revspin.net webpage.
-    Contains a single method "start".
+    Contains a single function "run".
   """
 
-  defdelegate start, to: Revspin.Processor, as: :process
+  defdelegate run, to: Revspin.Processor, as: :process
 end
